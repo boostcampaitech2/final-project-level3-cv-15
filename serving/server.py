06775f0d3d5c69a47c35947f8fa2c8e3b21faab4
saberhark <34380276+saberhark@users.noreply.github.com>
2021-12-11 20:47:44 +0900

    코드 정리 & Image 복구

--- serving/server.py
+++ serving/server.py
@@ -11,7 +11,7 @@ import torch
 import base64
 import random
 
-from help_funcs import get_stream_cam
+from help_funcs import *
 
 app = FastAPI()
 templates = Jinja2Templates(directory = 'templates')
@@ -42,16 +42,57 @@ def about_us(request: Request):
 	Display about us page
 	'''
 
-	return templates.TemplateResponse('about.html', 
+	return templates.TemplateResponse('about.html',
 			{"request": request})
 
-
-def video_streaming(model_name, img_size):
-	return get_stream_cam(model_name, img_size)
 
 ##############################################
 #------------POST Request Routes--------------
 ##############################################
+@app.post("/")
+async def detect_via_web_form(request: Request,
+							file_list: List[UploadFile] = File(...),
+							model_name: str = Form(...),
+							img_size: int = Form(640)):
+
+	'''
+	Requires an image file upload, model name (ex. yolov5s). Optional image size parameter (Default 640).
+	Intended for human (non-api) users.
+	Returns: HTML template render showing bbox data and base64 encoded image
+	'''
+
+	#assume input validated properly if we got here
+	if model_dict[model_name] is None:
+		model_dict[model_name] = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
+
+	img_batch = [cv2.imdecode(np.fromstring(await file.read(), np.uint8), cv2.IMREAD_COLOR)
+					for file in file_list]
+
+	#.copy() because the images are modified when running model, and we need originals when drawing bboxes later
+	results = model_dict[model_name](img_batch.copy(), size = img_size)
+
+	json_results = results_to_json(results, model_dict[model_name])
+
+	img_str_list = []
+	#plot bboxes on the image
+	for img, bbox_list in zip(img_batch, json_results):
+		for bbox in bbox_list:
+			label = f'{bbox["class_name"]} {bbox["confidence"]:.2f}'
+			plot_one_box(bbox['bbox'], img, label=label,
+					color=colors[int(bbox['class'])], line_thickness=3)
+
+		img_str_list.append(base64EncodeImage(img))
+
+	#escape the apostrophes in the json string representation
+	encoded_json_results = str(json_results).replace("'",r"\'").replace('"',r'\"')
+
+	return templates.TemplateResponse('show_results.html', {
+			'request': request,
+			'bbox_image_data_zipped': zip(img_str_list,json_results), #unzipped in jinja2 template
+			'bbox_data_str': encoded_json_results,
+		})
+
+
 @app.get("/video_feed")
 async def detect_via_web_form():
 	model_name = 'yolov5s'
@@ -62,7 +103,7 @@ async def detect_via_web_form():
 	Returns: HTML template render showing bbox data and base64 encoded image
 	'''
 
-	return StreamingResponse(video_streaming(model_name, img_size), media_type = "multipart/x-mixed-replace; boundary=frame")
+	return StreamingResponse(get_stream_cam(model_name, img_size), media_type="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.post("/detect/")
@@ -110,47 +151,6 @@ async def detect_via_api(request: Request,
 
 	return json_results
 	
-##############################################
-#--------------Helper Functions---------------
-##############################################
-
-def results_to_json(results, model):
-	''' Converts yolo model output to json (list of list of dicts)'''
-	return [
-				[
-					{
-					"class": int(pred[5]),
-					"class_name": model.model.names[int(pred[5])],
-					"bbox": [int(x) for x in pred[:4].tolist()], #convert bbox results to int from float
-					"confidence": float(pred[4]),
-					}
-				for pred in result
-				]
-			for result in results.xyxy
-			]
-
-
-def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
-	# Directly copied from: https://github.com/ultralytics/yolov5/blob/cd540d8625bba8a05329ede3522046ee53eb349d/utils/plots.py
-    # Plots one bounding box on image 'im' using OpenCV
-    assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
-    tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-
-
-def base64EncodeImage(img):
-	''' Takes an input image and returns a base64 encoded string representation of that image (jpg format)'''
-	_, im_arr = cv2.imencode('.jpg', img)
-	im_b64 = base64.b64encode(im_arr.tobytes()).decode('utf-8')
-
-	return im_b64
 
 if __name__ == '__main__':
 	import uvicorn
