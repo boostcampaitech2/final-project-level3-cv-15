b591394158708c9075d6996a9593bfd37ab58795
saberhark <34380276+saberhark@users.noreply.github.com>
2021-12-20 05:29:07 +0900

    front(스트림 버튼, 신호등, 신호 시간 변경, 신호 변경 등) server > jinjatemplate 변경(static 폴더)

---
+++ serving/static/js/functions.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    $("#btn_stream").click(function () {
+        document.getElementById("streaming").src = "/video_feed"
+        document.getElementById("traffic").src = "/static/src/img/red.jpg"
+
+        traffic()
+    })
+}
