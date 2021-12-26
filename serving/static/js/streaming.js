b67722205ab250e66c67b43078a9976a6f02f4a0
saberhark <34380276+saberhark@users.noreply.github.com>
2021-12-27 01:43:22 +0900

    동영상 업로드

--- serving/static/js/streaming.js
+++ serving/static/js/streaming.js
@@ -1,10 +1,13 @@
-function streaming(){
+function streaming(file_path=""){
     var set_up = false
     var traffic = traffic_control()
 
-    var source = new EventSource("http://34.64.139.254:8000/video_feed");
-    // var source = new EventSource("http://localhost:8000/video_feed");
-    
+    url = "http://34.64.139.254:8000/video_feed"
+    //url = "http://localhost:8000/video_feed"
+    if (file_path != "") url += "?file_path="+file_path
+
+    var source = new EventSource(url);
+
     source.onmessage = function(event) {
         var data = JSON.parse(event.data)
 
@@ -12,49 +15,6 @@ function streaming(){
             traffic.traffic_start()
             set_up=true
         }
-        console.log(data.inform.traffic)
-        if (data.ret === false){
-            document.getElementById("log-1").innerHTML = "횡단보도 : 미감지";
-            source.close()
-        }
-
-        document.getElementById("streaming").src = "data:image/jpeg;base64, " + data.img;
-
-        document.getElementById("log-1").innerHTML = "횡단보도 : 감지";
-        document.getElementById("log-2").innerHTML = '감지된 박스<br>'+JSON.stringify(data.inform.log.all_box);
-        document.getElementById("log-3").innerHTML = '횡단보도 내 박스<br>'+JSON.stringify(data.inform.log.roi_box);
-
-        if (data.inform.traffic.keep_green === true)
-            traffic.set_keep_green(true)
-        else
-            traffic.set_keep_green(false)
-
-        if (data.inform.traffic.keep_red === true)
-            traffic.set_keep_red(true)
-        else
-            traffic.set_keep_red(false)
-
-    };
-}
-
-function streaming_upload(FileList){
-    var set_up = false
-    var traffic = traffic_control()
-
-    console.log(FileList)
-    console.log(typeof(FileList))
-    alert(FileList)
-
-    // var source = new SSE("http://0.0.0.0:8000/video_feed", {file_list: FileList});
-    var source = new SSE("http://localhost:8000/video_feed", {file_list: FileList});
-    source.onmessage = function(event) {
-        var data = JSON.parse(event.data)
-
-        if (!set_up) {
-            traffic.traffic_start()
-            set_up=true
-        }
-        console.log(data.inform.traffic)
         if (data.ret === false){
             document.getElementById("log-1").innerHTML = "횡단보도 : 미감지";
             source.close()
